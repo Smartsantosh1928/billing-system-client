c8fb6dea8cf1363ebbc5012405503b780152ca18
test <test@UGSERVER.com>
2023-12-26 15:52:19 +0530

    home

--- src/pages/Dashboard/Home.jsx
+++ src/pages/Dashboard/Home.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 export function Home() {
   return (
-    <div className='font-pop'>
-      Home
+    <div className='flex gap-5 font-pop '>
+      <div className='relative h-[80px] w-[300px] rounded-xl bg-gray-200 shadow-xl'>
+        <h1 className='absolute bottom-0 left-0 mb-2 flex ml-2 ' >Total Products</h1>
+      </div>
+      <div className='relative h-[80px] w-[300px] rounded-xl bg-red-200 shadow-xl'>
+        <h1 className='absolute bottom-0 left-0 mb-2 flex ml-2 ' >Low Stocks</h1>
+      </div>
+      <div className='relative h-[80px] w-[300px] rounded-xl bg-blue-300 shadow-xl'>
+        <h1 className='absolute bottom-0 left-0 mb-2 flex ml-2 ' >Total Customers</h1>
+      </div>
+      <div className='relative h-[80px] w-[300px] rounded-xl bg-green-400 shadow-xl'>
+        <h1 className='absolute bottom-0 left-0 mb-2 flex ml-2 ' >Total Revenue</h1>
+      </div>
     </div>
   )
 }
