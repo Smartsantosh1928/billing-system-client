278c20104574b4b437bc1f2f3ed0191cea6c36aa
test <test@UGSERVER.com>
2024-01-19 11:13:30 +0530

    new

--- src/pages/Auth/NewLogin.jsx
+++ src/pages/Auth/NewLogin.jsx
@@ -1,22 +1,37 @@
 import React from 'react'
-import{UserIcon } from "@heroicons/react/24/solid";
+import{UserCircleIcon} from "@heroicons/react/24/solid";
+import google from "../../../public/google.png"
 
 export function NewLogin() {
   return (
    <>
-   <div className='flex justify-center mt-30 pt-24'>
-        <div className='flex justify-end h-[50%] p-12 rounded-lg w-[60%] bg-gray-400 '>
-             <div className='flex flex-col items-center pt-10 h-[380px]  w-[300px] rounded-xl  bg-white'>
-               
-                   <h1 className='text-orange-400 text-lg text-center font-semibold '>Welcome</h1>
-                   <div className=' h-[30%] w-[70%] '>
-                        <input type="text" name='email' placeholder='Enter Email' className='mb-3 border-[2px] mt-7 border-gray-500  active:border-orange-400  rounded-full placeholder:text-sm placeholder:indent-2 active:indent-2' />
-                        <input type="text" placeholder='Enter PassWord' className='border-2 border-gray-500  active:border-orange-400  rounded-full placeholder:text-sm placeholder:indent-2 active:indent-2'/> 
-                   </div>
-                   <button className='mt-4  h-7 w-24 bg-orange-400 rounded-3xl text-sm hover:border-2 font-semibold transform duration-300 hover:bg-transparent hover:border-orange-400' >Submit</button> 
-             </div>
-             <hr />or<hr />
-        </div>
+   <div className=' w-full  h-screen flex items-center justify-center '>
+          <div className='flex justify-end h-[70%] p-12 rounded-lg w-[60%] bg-gray-400 '>
+               <div className='flex flex-col items-center justify-center h-[380px]  w-[300px] rounded-xl  bg-white shadow-xl'>  
+                    <h1 className='text-orange-400 text-lg text-center font-semibold '>Welcome</h1>
+                    <div className=' h-[35%] w-[70%] '>
+                         <span className='text-sm text-gray-600'>Email</span>
+                         <input type="text" name='email' placeholder='Enter Email' className='text-sm cursor-pointer w-full h-[25px] mb-3 border-[2px]  border-gray-500  rounded-full placeholder:text-sm placeholder:indent-2  indent-2 ' />
+                         <span className='text-sm text-gray-600'>Password</span>
+                         <input type="text" placeholder='Enter PassWord' className=' cursor-pointer w-full h-[25px] text-sm border-2 border-gray-500  rounded-full placeholder:text-sm placeholder:indent-2 indent-2'/> 
+                    </div>
+                    <div >
+                         <button className=' h-7 w-24 bg-orange-400 rounded-3xl text-sm hover:border-2 font-semibold transform duration-300 hover:bg-transparent hover:border-orange-400' >Login</button> 
+                    </div>
+                    <div className='flex items-center pl-2 w-[70%] pt-3'>
+                         <hr className='w-[40%] border-1 border-gray-500 ' />
+                         <p className='px-2 ' >or</p>
+                         <hr className='w-[40%] border-1 border-gray-500 ' />
+                    </div>
+                    <div>
+                         <img src={google} className='h-10 w-10 cursor-pointer' alt="" />
+                    </div>
+                    <div className='flex  w-[70%] '>
+                         <p className='text-sm cursor-pointer'>Don't have an account ?</p>
+                         <p className='text-sm text-orange-400 hover:text-blue-400 hover:cursor-pointer font-bold px-1' >Signin</p>
+                    </div>
+               </div>
+          </div>
    </div>
    </>
   )
