1067a8dc4d2a28d36b9d92a7c4ba66879ab746ef
test <test@UGSERVER.com>
2024-01-19 12:03:37 +0530

    button

--- src/pages/Home/NavBar.jsx
+++ src/pages/Home/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Button}from '@material-tailwind/react'
 
 const NavBar = () => {
     const button = 'h-10 w-24 bg-blue-300 rounded-3xl text-lg hover:border-2 transform duration-300 hover:bg-transparent hover:border-blue-300'
@@ -10,8 +11,8 @@ const NavBar = () => {
            <h1 className='text-xl font-extrabold'>Ziry Tech</h1>
         </div>
         <div className='flex w-full justify-end items-center gap-2  pr-5'> 
-            <button className={button}>Signin</button>
-            <button className={button}>Signup</button>
+            <Button variant="outlined"  >Signin</Button>
+            <Button variant="gradient"  className={""}>Signup</Button>
         </div>
     </div>
     </>
